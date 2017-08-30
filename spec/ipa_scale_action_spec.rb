c1cfde2af3cfefbd91d2066915132176bcf4e5ac
Danilo Caetano <dcaetano@truecar.com>
2017-08-29 22:17:17 -0600

    TODO specs + sample ipas

--- spec/ipa_scale_action_spec.rb
+++ spec/ipa_scale_action_spec.rb
@@ -1,9 +1,10 @@
-describe Fastlane::Actions::IpaScaleAction do
-  describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The ipa_scale plugin is working!")
-
-      Fastlane::Actions::IpaScaleAction.run(nil)
-    end
-  end
-end
+# TODO: Include spec with small.ipa, medium.ipa, large.ipa to show different outputs
+# describe Fastlane::Actions::IpaScaleAction do
+#   describe '#run' do
+#     it 'prints a message' do
+#       expect(Fastlane::UI).to receive(:message).with("The ipa_scale plugin is working!")
+#
+#       Fastlane::Actions::IpaScaleAction.run(nil)
+#     end
+#   end
+# end
